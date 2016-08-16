ee6d66d0544c9aedb7f307729376bd0c92637c58
leMaik <an@maik.tk>
2016-08-16 23:19:54 +0200

    Add a default config and rename the plugin and the service class.

--- plugin/src/main/java/de/craften/plugins/playerdatastore/plugin/PlayerDataStorePlugin.java
+++ plugin/src/main/java/de/craften/plugins/playerdatastore/plugin/PlayerDataStorePlugin.java
@@ -1,7 +1,7 @@
 package de.craften.plugins.playerdatastore.plugin;
 
 import de.craften.plugins.playerdatastore.api.PlayerDataStore;
-import de.craften.plugins.playerdatastore.api.PlayerDataStoreProvider;
+import de.craften.plugins.playerdatastore.api.PlayerDataStoreService;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,15 +16,17 @@ public class PlayerDataStorePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
         try {
-            jedis = new Jedis(new URI(getConfig().getString("jedisUri")));
+            jedis = new Jedis(new URI(getConfig().getString("redisUrl")));
         } catch (URISyntaxException e) {
-            getLogger().severe("Invalid Redis URI " + getConfig().getString("jedisUri", "<no uri set>"));
+            getLogger().severe("Invalid Redis URI " + getConfig().getString("redisUrl"));
             getServer().getPluginManager().disablePlugin(this);
         }
 
-        getServer().getServicesManager().register(PlayerDataStoreProvider.class,
-                new PlayerDataStoreProvider() {
+        getServer().getServicesManager().register(PlayerDataStoreService.class,
+                new PlayerDataStoreService() {
                     @Override
                     public PlayerDataStore getStore(OfflinePlayer player) {
                         return new RedisPlayerDataStore(jedis, player.getUniqueId());
